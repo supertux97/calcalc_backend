4a6717b44088d4f2a40175235780799a954c6590
Erlend Østlie <erlend.oestlie@gmail.com>
2025-08-29 14:06:49 +0200

    fix test action

--- TestProject/tests/obj/Debug/net9.0/tests.AssemblyInfo.cs
+++ TestProject/tests/obj/Debug/net9.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58d34119c50887bb4ac6600225b8d6484801adc")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
